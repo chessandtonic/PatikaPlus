fb81266579184d4d7c53554c9ac806a3a82b07f0
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-09-13 17:40:16 +0300

    leap

---
+++ src/week1/LeapYearFinder.java
@@ -0,0 +1,24 @@
+package week1;
+
+import java.util.Random;
+import java.util.Scanner;
+// Arkadaşlar selam, kodu test etmek için PatikaPlus altında bulabileceğiniz Main.java programını kullanabilirsiniz.
+// Temelde Main.java'da olduğu gibi bu programı başka bir main method'u ile çağırarak çalıştırabilirsiniz.
+
+public class LeapYearFinder {
+    public static void leapYear() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Get input from the user
+        System.out.print("Please enter year: ");
+        int year = scanner.nextInt();
+
+        /* Leap Year Calculation; true if the year is divisible by 4 or by 100 and 400. */
+        if ((year % 4 == 0) || (year % 100 == 0 && year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
