7ffcf84f34e4820cec43dc6db8af2d800b5a5427
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-09-13 11:10:17 +0300

    daireAlan

---
+++ src/week1/DaireAlan.java
@@ -0,0 +1,22 @@
+package week1;
+
+import java.util.Scanner;
+// Arkadaşlar selam, kodu test etmek için PatikaPlus altında bulabileceğiniz Main.java programını kullanabilirsiniz.
+// Temelde Main.java'da olduğu gibi bu programı başka bir main method'u ile çağırarak çalıştırabilirsiniz.
+public class DaireAlan {
+    public static void main(String[] args) {
+    int r;
+    double alpha, pi = 3.14;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Dairenin yarıçapı: ");
+    r = input.nextInt();
+    System.out.print("Merkez açısının ölçüsü: ");
+    alpha = input.nextDouble();
+
+    double alan = (pi * (r * r) * alpha) / 360;
+
+    System.out.println("Dairenin alanı:" + alan);
+    }
+}
