78d4fce37475fe55a39954ec827f880eb5b2ecfb
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-09-14 09:49:50 +0300

    tersUcgen

--- src/week1/TersUcgen.java
+++ src/week1/TersUcgen.java
@@ -17,3 +17,29 @@ public class TersUcgen {
         }
     }
 }
+/* Another solution
+public class Main {
+        public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+
+                // Variable identification
+                int number;
+
+                // Retrieving data from the user
+                System.out.print("Please enter a number:");
+                number = input.nextInt();
+
+                // Calculation Section
+
+                for ( int i = number; i >= 1; i--) {
+                        for (int k = 1;k <= (number-i); k++) {
+                                System.out.print(" ");
+                        }
+                        for (int j = 1; j <= (2*i) - 1; j++ ) {
+                                System.out.print("*");
+                        }
+                        System.out.println();
+                }
+        }
+}
+*/
