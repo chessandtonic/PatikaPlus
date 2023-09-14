b4f74ef1b27b02ef708cbf51e5acb5458ceb632c
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-09-14 23:30:39 +0300

    digitSum

---
+++ src/week1/DigitSumCalculator.java
@@ -0,0 +1,30 @@
+package week1;
+
+import java.util.Scanner;
+// Arkadaşlar selam, kodu test etmek için PatikaPlus altında bulabileceğiniz Main.java programını kullanabilirsiniz.
+// Temelde Main.java'da olduğu gibi bu programı başka bir main method'u ile çağırarak çalıştırabilirsiniz.
+public class DigitSumCalculator {
+    // Recursive function to calculate the sum of digits
+    public static int sumOfDigits(int number) {
+        // Base case: If the number is 0, return 0 (no digits to sum)
+        if (number == 0) {
+            return 0;
+        }
+        // Recursive case: Sum the last digit and call the function for the remaining part of the number
+        return number % 10 + sumOfDigits(number / 10);
+    }
+
+    public static void digitsSum(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = scanner.nextInt();
+
+        // Calculate the sum of digits using the recursive function
+        int sum = sumOfDigits(n);
+
+        System.out.println("Sum of digits: " + sum);
+
+        scanner.close();
+    }
+}
