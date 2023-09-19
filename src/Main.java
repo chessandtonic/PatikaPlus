997eb9e011ff524f6bd10f4775b7344e7e69df98
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-09-19 14:56:22 +0300

    updated main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import week1.*;
+import week2.*;
+
 public class Main {
-    public static void main(String[] args) {
-        BMICalculator.calculate();
+    public static void main(String[] args) { PalindromeFinder.PalFinder();
     }
 }
