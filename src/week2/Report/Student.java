8cdaf9a2f8df0732cdec5919b2f65ebbe91275fa
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-09-21 14:49:44 +0300

    report

---
+++ src/week2/Report/Student.java
@@ -0,0 +1,90 @@
+package week2.Report;
+
+public class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+
+    }
+    public void addBulkPerformance(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.perf = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.perf = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.perf = kimya;
+        }
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        this.avarage = (((mat.note * mat.weightExam) + (mat.perf * mat.weightPerf)) +
+                ((fizik.note * fizik.weightExam) + (fizik.perf * fizik.weightPerf)) +
+                ((kimya.note * kimya.weightExam) + (kimya.perf * kimya.weightPerf))) / 3;
+    }
+
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote(){
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+    }
+
+}
