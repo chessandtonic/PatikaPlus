bdbad021de13edcafb79cd577474bfb7aa77d781
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-09-22 17:03:49 +0300

    MinMax

---
+++ src/week2/MinMaxValues.java
@@ -0,0 +1,44 @@
+package week2;
+
+import java.util.Scanner;
+
+public class MinMaxValues {
+    public static void main(String[] args) {
+        int target;
+
+        // Getting input from the user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a target number: ");
+        target = input.nextInt();
+
+        int[] list = {56, 34, 1, 8, 101, -2, -33};
+
+        int minDiff = Integer.MAX_VALUE; // Initialize minDiff with a large value
+        int maxDiff = Integer.MIN_VALUE; // Initialize maxDiff with a small value
+        int minIndex = -1; // Initialize minIndex to an invalid value
+        int maxIndex = -1; // Initialize maxIndex to an invalid value
+
+        for (int i = 0; i < list.length; i++) {
+            int difference = Math.abs(list[i] - target);
+
+            if (difference < minDiff) {
+                minDiff = difference;
+                minIndex = i;
+            }
+
+            if (difference > maxDiff) {
+                maxDiff = difference;
+                maxIndex = i;
+            }
+        }
+
+        System.out.println("Nearest number to " + target + " is: " + list[minIndex]);
+        System.out.println("Farthest number from " + target + " is: " + list[maxIndex]);
+
+        input.close();
+    }
+}
+
+
+
+
