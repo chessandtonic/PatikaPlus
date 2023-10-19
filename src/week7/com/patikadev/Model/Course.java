57c7950c3895bfb0e13cee99efdbfa2cc8be2866
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-10-20 01:34:52 +0300

    sixth

---
+++ src/week7/com/patikadev/Model/Course.java
@@ -0,0 +1,9 @@
+package week7.com.patikadev.Model;
+
+public class Course {
+    private int id;
+    private int user_id;
+    private int patika_id;
+    private String name;
+    private String lang;
+}
