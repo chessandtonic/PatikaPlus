eff0cc4ad2c9f17a76748dc8400bf90ba14d3890
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-10-19 19:37:09 +0300

    fifth

--- src/week7/com/patikadev/View/UpdatePatikaGUI.java
+++ src/week7/com/patikadev/View/UpdatePatikaGUI.java
@@ -5,27 +5,42 @@ import week7.com.patikadev.Helper.Helper;
 import week7.com.patikadev.Model.Patika;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class UpdatePatikaGUI extends JFrame{
+public class UpdatePatikaGUI extends JFrame {
     private JPanel wrapper;
     private JTextField field_pathName;
     private JButton update_Button;
     private Patika patika;
-    public UpdatePatikaGUI(Patika patika){
+
+    public UpdatePatikaGUI(Patika patika) {
         this.patika = patika;
         add(wrapper);
-        setSize(300,150);
+        setSize(300, 150);
         setLocation(Helper.screenCenterPoint("x", getSize()), Helper.screenCenterPoint("y", getSize()));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle(Config.PROJECT_TITLE);
         setVisible(true);
 
         field_pathName.setText(patika.getName());
+        update_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Helper.isFieldEmpty(field_pathName)) {
+                    Helper.showMsg("fill");
+                } else {
+                    if (Patika.update(patika.getId(), field_pathName.getText())) {
+                        Helper.showMsg("done");
+                    }
+                    dispose();
+                }
+            };
+        });
     }
-
-    public static void main(String[] args) {
-        Helper.setLayout();
-        Patika p = new Patika(1, "Frontend");
-        UpdatePatikaGUI updatePatikaGUI = new UpdatePatikaGUI(p);
+    public static void main (String[]args){
+            Helper.setLayout();
+            Patika p = new Patika(1, "Frontend");
+            UpdatePatikaGUI updatePatikaGUI = new UpdatePatikaGUI(p);
     }
 }
