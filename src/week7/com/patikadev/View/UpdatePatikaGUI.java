1746690972087c838904ced3dd384afb55c7b1d5
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-10-19 19:13:04 +0300

    fifth

--- src/week7/com/patikadev/View/UpdatePatikaGUI.java
+++ src/week7/com/patikadev/View/UpdatePatikaGUI.java
@@ -1,4 +1,22 @@
 package week7.com.patikadev.View;
 
-public class UpdatePatikaGUI {
+import week7.com.patikadev.Helper.Config;
+import week7.com.patikadev.Helper.Helper;
+import week7.com.patikadev.Model.Patika;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UpdatePatikaGUI extends JFrame{
+    private JPanel wrapper;
+    private Patika patika;
+    public UpdatePatikaGUI(Patika patika){
+        this.patika = patika;
+        add(wrapper);
+        setSize(300,150);
+        setLocation(Helper.screenCenterPoint("x", getSize()), Helper.screenCenterPoint("y", getSize()));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle(Config.PROJECT_TITLE);
+        setVisible(true);
+    }
 }
