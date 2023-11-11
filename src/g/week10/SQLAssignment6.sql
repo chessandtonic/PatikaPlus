9c648ac027b071b903d45b8de6a1f96c95c3bcc1
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-11-11 21:51:02 +0300

    SQL Assignment 6

--- src/g/week10/SQLAssignment6.sql
+++ src/g/week10/SQLAssignment6.sql
@@ -1,2 +1,13 @@
 Ödev 6:
-1. SELECT AVG(rental_rate) FROM film
+
+1.  '2,98'
+    SELECT AVG(rental_rate) FROM film;
+2.  '92'
+    SELECT COUNT(*) FROM film
+    WHERE title LIKE 'C%';
+3.  '184'
+    SELECT MAX(length) FROM film
+    WHERE rental_rate = 0.99;
+4.  '21'
+    SELECT COUNT(DISTINCT replacement_cost) FROM film
+    WHERE length > 150;
