861f495ed6ae04d4bcd054bc46065f9a47a1ae6c
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-11-12 20:55:42 +0300

    SQL Assignment 11

---
+++ src/g/week10/SQLAssignment11.sql
@@ -0,0 +1,31 @@
+Ödev 11:
+
+1.  (
+    SELECT first_name FROM actor
+    )
+     UNION
+    (
+     SELECT first_name FROM customer
+    );
+2.  (
+    SELECT first_name FROM actor
+    )
+     INTERSECT
+    (
+     SELECT first_name FROM customer
+    );
+3.  (
+    SELECT first_name FROM actor
+    )
+    EXCEPT
+    (
+    SELECT first_name FROM customer
+    );
+4.  (
+    SELECT first_name FROM actor
+	ORDER BY first_name
+    )
+    UNION ALL
+    (
+    SELECT first_name FROM customer
+    );
