a7d1796ce9e13887d5ef3c46a7fb83368832d680
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-11-13 00:33:46 +0300

    SQL Assignment 12

--- src/g/week10/SQLAssignment12.sql
+++ src/g/week10/SQLAssignment12.sql
@@ -9,6 +9,8 @@
 3.  SELECT * FROM film
     WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
       AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
-4.  SELECT customer_id, COUNT(payment_id) FROM payment
-    GROUP BY customer_id
-    ORDER BY COUNT (payment_id) DESC;
+4.  SELECT customer.customer_id, customer.first_name, customer.last_name, COUNT(payment.payment_id) FROM customer
+    JOIN payment ON customer.customer_id = payment.customer_id
+    GROUP BY customer.customer_id
+    ORDER BY COUNT(payment.payment_id) DESC;
+
