c3b72538d4f8728a6becd6d4ce4ac851e6399074
Cüneyt Can İşcan <c.caniscan@gmail.com>
2023-11-13 12:29:15 +0300

    SQL Assignment 12

--- src/g/week10/SQLAssignment2.sql
+++ src/g/week10/SQLAssignment2.sql
@@ -1,7 +1,7 @@
 Ödev 2:
 
 1.  SELECT * FROM film
-    WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+    WHERE replacement_cost BETWEEN 12.99 AND 16.98;
 2.  SELECT first_name, last_name FROM actor
     WHERE first_name IN ('Penelope', 'Nick', 'Ed');
 3.  SELECT * FROM film
